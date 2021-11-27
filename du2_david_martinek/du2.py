2668d044d521b94d2c8e3c1deed0765ae5ffbb8d
martineda96 <93780022+martineda96@users.noreply.github.com>
2021-11-27 11:10:32 +0100

    Add files via upload

--- du2_david_martinek/du2.py
+++ du2_david_martinek/du2.py
@@ -1,6 +1,12 @@
 import csv
 
-with open("test.csv", encoding="utf-8") as csvinfile,\
+def zapis(prumer,den,radek, writer):
+    prumer /= den
+    prumer = float("{0:.4f}".format(prumer))
+    radek[5] = prumer
+    writer.writerow(radek)
+
+with open("Data_Libechovka.csv", encoding="utf-8") as csvinfile,\
      open("test_out7.csv", "w", encoding="utf-8") as csvoutfile7,\
      open("test_out365.csv", "w", encoding="utf-8") as csvoutfilerok:
     reader = csv.reader(csvinfile, delimiter =",")
@@ -19,19 +25,13 @@ with open("test.csv", encoding="utf-8") as csvinfile,\
         den_7 += 1
         prumer_7 += float(row[5])
         if den_7 == 7:
-            prumer_7 /= den_7
-            prumer_7 = float("{0:.4f}".format(prumer_7))
-            radek_7[5] = prumer_7
-            writer7.writerow(radek_7)
+            zapis(prumer_7,den_7,radek_7,writer7)
             den_7 = 0
             prumer_7 = 0
         if aktualni_rok != row[2]:
             aktualni_rok = row[2]
             if den_rok != 0:
-                prumer_rok /= den_rok
-                prumer_rok = float("{0:.4f}".format(prumer_rok))
-                radek_rok[5] = prumer_rok
-                writerrok.writerow(radek_rok)
+                zapis(prumer_rok,den_rok,radek_rok,writerrok)
                 den_rok = 0
                 prumer_rok = 0
             radek_rok = row
@@ -39,12 +39,6 @@ with open("test.csv", encoding="utf-8") as csvinfile,\
         prumer_rok += float(row[5])
     
     if den_7 != 0:
-        prumer_7 /= den_7
-        prumer_7 = float("{0:.4f}".format(prumer_7))
-        radek_7[5] = prumer_7
-        writer7.writerow(radek_7)
+        zapis(prumer_7,den_7,radek_7,writer7)
     #Cyklus výše nikdy nezpracuje všechna data průtoků za rok. Tady se vypíše zbytek informace za poslední rok. 
-    prumer_rok /= den_rok
-    prumer_rok = float("{0:.4f}".format(prumer_rok))
-    radek_rok[5] = prumer_rok
-    writerrok.writerow(radek_rok)
+    zapis(prumer_rok,den_rok,radek_rok,writerrok)
