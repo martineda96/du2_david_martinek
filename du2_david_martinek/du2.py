dbe8dade3fb91111cc7604ed72c19c19437bc12b
martineda96 <93780022+martineda96@users.noreply.github.com>
2021-11-27 09:22:53 +0100

    Add files via upload

--- du2_david_martinek/du2.py
+++ du2_david_martinek/du2.py
@@ -7,19 +7,32 @@ with open("test.csv", encoding="utf-8") as csvinfile,\
     writer7 = csv.writer(csvoutfile7)
     writer365 = csv.writer(csvoutfile365)
 
-    aktualni_den = 0
+    den_7 = 0
     prumer_7 = 0
-    aktualni_rok = 0
+    den_rok = 0
     prumer_rok = 0
 
     for row in reader:
-        if aktualni_den % 7 == 0:
+        if den_7 == 0:
             radek_7 = row
+        den_7 += 1
         prumer_7 += float(row[5])
+        if den_7 == 6:
+            prumer_7 /= (den_7+1)
+            radek_7[5] = prumer_7
+            writer7.writerow(radek_7)
+            den_7 = 0
+            prumer_7 = 0
+    
+    if prumer_7 != 0:
+        prumer_7 /= (den_7+1)
+        radek_7[5] = prumer_7
+        writer7.writerow(radek_7)
+
+
         '''if aktualni_rok != row[2]:
             aktualni_rok = row[2]
             radek_rok[5] = prumer_rok/
             writer365.writerow(radek_rok)
             radek_rok = row
             #a něco se stane'''
-        aktualni_den += 1
