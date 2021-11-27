d240a5f02e902cb00666e15478040c039f236d62
martineda96 <93780022+martineda96@users.noreply.github.com>
2021-11-27 10:56:54 +0100

    Add files via upload

--- du2_david_martinek/du2.py
+++ du2_david_martinek/du2.py
@@ -6,7 +6,7 @@ with open("test.csv", encoding="utf-8") as csvinfile,\
     reader = csv.reader(csvinfile, delimiter =",")
     writer7 = csv.writer(csvoutfile7)
     writerrok = csv.writer(csvoutfilerok)
-
+    
     den_7 = 0
     prumer_7 = 0
     aktualni_rok = 0
@@ -19,15 +19,17 @@ with open("test.csv", encoding="utf-8") as csvinfile,\
         den_7 += 1
         prumer_7 += float(row[5])
         if den_7 == 7:
-            prumer_7 /= (den_7)
+            prumer_7 /= den_7
+            prumer_7 = float("{0:.4f}".format(prumer_7))
             radek_7[5] = prumer_7
             writer7.writerow(radek_7)
             den_7 = 0
             prumer_7 = 0
         if aktualni_rok != row[2]:
             aktualni_rok = row[2]
-            if prumer_rok != 0:
-                prumer_rok /= (den_rok)
+            if den_rok != 0:
+                prumer_rok /= den_rok
+                prumer_rok = float("{0:.4f}".format(prumer_rok))
                 radek_rok[5] = prumer_rok
                 writerrok.writerow(radek_rok)
                 den_rok = 0
@@ -36,7 +38,13 @@ with open("test.csv", encoding="utf-8") as csvinfile,\
         den_rok += 1
         prumer_rok += float(row[5])
     
-    if prumer_7 != 0:
-        prumer_7 /= (den_7+1)
+    if den_7 != 0:
+        prumer_7 /= den_7
+        prumer_7 = float("{0:.4f}".format(prumer_7))
         radek_7[5] = prumer_7
         writer7.writerow(radek_7)
+    #Cyklus výše nikdy nezpracuje všechna data průtoků za rok. Tady se vypíše zbytek informace za poslední rok. 
+    prumer_rok /= den_rok
+    prumer_rok = float("{0:.4f}".format(prumer_rok))
+    radek_rok[5] = prumer_rok
+    writerrok.writerow(radek_rok)
